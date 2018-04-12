2b55c2b04d9ee5bf7d51c0885005a07a68650a70
Lucas-Luwa <League@AdministorsiMac.attlocal.net>
2018-04-11 18:27:36 -0700

    4/11/18(Save 2)
    DIY ARRAYLIST(IP)

--- src/_02_diy_array_list/ArrayList.java
+++ src/_02_diy_array_list/ArrayList.java
@@ -2,8 +2,20 @@ package _02_diy_array_list;
 
 public class ArrayList<T> {
 	T[] list;
-	
-	ArrayList(T[] list) {
-		This.list = list;
+
+	ArrayList() {
+		T[] newArray = (T[]) new Object[0];
 	}
+
+	T[] add(T number) {
+		list[list.length] = number;
+		return list;
+	}
+
+	T[] get(T value) {
+		T get = list(value);
+		return  get;
+	}
+
+
 }
