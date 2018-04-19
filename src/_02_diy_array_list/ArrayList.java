1366fe30b1f751e964f72d8576374a12bd5e4d36
Lucas-Luwa <League@AdministorsiMac.attlocal.net>
2018-04-18 18:21:47 -0700

    4/18/18
    Old Fashioned Arrays(Complete)

--- src/_02_diy_array_list/ArrayList.java
+++ src/_02_diy_array_list/ArrayList.java
@@ -1,21 +1,65 @@
 package _02_diy_array_list;
 
+import java.util.Set;
+
 public class ArrayList<T> {
-	T[] list;
+	T[] list = (T[]) new Object[0];;
+
+	public static void main(String[] args) {
+	}
 
 	ArrayList() {
 		T[] newArray = (T[]) new Object[0];
 	}
 
-	T[] add(T number) {
-		list[list.length] = number;
-		return list;
+	void add(T number) {
+		T[] newArray = (T[]) new Object[list.length + 1];
+
+		for (int j = 0; j < list.length; j++) {
+			newArray[j] = list[j];
+		}
+		newArray[newArray.length - 1] = number;
+		list = newArray;
 	}
 
-	T[] get(T value) {
-		T get = list(value);
-		return  get;
+	T get(int value) {
+		T x = list[value];
+		return x;
 	}
 
+	void set(int number, T value) {
+		list[2] = value;
+	}
 
+	void addAt(int number, T value) {
+		T[] newArray = (T[]) new Object[list.length + 1];
+		for (int j = 0; j < number; j++) {
+			newArray[j] = list[j];
+		}
+		newArray[number] = value;
+		for (int i = number + 1; i < list.length + 1; i++) {
+			newArray[i] = list[i - 1];
+		}
+		list = newArray;
+	}
+
+	void remove(int number) {
+		T[] newArray = (T[]) new Object[list.length - 1];
+		for (int i = 0; i < number; i++) {
+			newArray[i] = list[i];
+		}
+		for (int i = number; i < list.length - 1; i++) {
+			newArray[i] = list[i + 1];
+		}
+		list = newArray;
+	}
+	boolean contains(String word) {
+		boolean value = false;
+		for (int i = 0; i < list.length; i++) {
+			if(list[i].equals(word)) {
+				value = true;
+			}
+		}
+		return value;
+	}
 }
